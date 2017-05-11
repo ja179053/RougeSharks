cf5938b2c8246ecdc89bb80af79c55b00027bac6
ja179053 <ja179053@falmouth.ac.uk>
2017-05-11 07:00:06 +0100

    First Build!
    Let's make a game!

---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Manager : MonoBehaviour {
+	public AudioMixer am;
+	public Slider s;
+	// Use this for initialization
+	void Start () {
+		am = Resources.Load ("MainAudioMixer") as AudioMixer;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.Escape)) {
+			if (Application.isEditor) {
+				Debug.Break ();
+			} else {
+				Application.Quit ();
+			}
+		}
+	}
+	public void UpdateVolume(){		
+		Debug.Log (GetComponent<AudioSource>().outputAudioMixerGroup.name);
+		am.SetFloat (GetComponent<AudioSource>().outputAudioMixerGroup.name, s.value);
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	Rigidbody r;
+	public float playerNumber;
+	public float setSpeed = 0.1f;
+	static float speed = 0;
+	static float dashCooldown = 2;
+	// Use this for initialization
+	void Start () {
+		r = GetComponent<Rigidbody> ();
+		if (setSpeed > speed) {
+			speed = setSpeed;
+		}
+	}
+	bool grounded;
+	public LayerMask ground;
+	// Update is called once per frame
+	void Update () {
+		Vector3 direction = new Vector3 (Input.GetAxis ("Horizontal" + playerNumber), 0, Input.GetAxis ("Vertical" + playerNumber));
+		transform.LookAt (transform.position + direction);
+		r.MovePosition(transform.position + (direction * speed));
+		if (Input.GetButton ("Dash" + playerNumber)) {
+			if (canDash) {
+				StartCoroutine (Dash ());
+			}
+		}
+		if (Input.GetButton ("Jump" + playerNumber)) {
+			if (canJump) {
+				StartCoroutine (Jump ());			
+			}
+		}
+		RaycastHit rh;
+		grounded = Physics.Raycast (transform.position, Vector3.down, out rh);
+	}
+	bool canJump = true;
+	IEnumerator Jump(){
+		r.MovePosition (Vector3.up);
+		canJump = false;
+		yield return new WaitUntil (() => grounded);
+		canJump = true;
+	}
+	bool canDash = true;
+	IEnumerator Dash(){
+		canDash = false;
+		r.MovePosition(transform.position + (transform.forward * 2));
+		yield return new WaitForSeconds (dashCooldown);
+		canDash = true;
+	}
+}
